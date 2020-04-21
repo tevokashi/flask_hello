35f5fd6a973524d7785129fc1158a8c6b87a4631
Stevan Nogueira <stevan.nogueira@mandic.net.br>
2020-04-20 21:01:36 -0300

    not found handler

--- app/api.py
+++ app/api.py
@@ -2,6 +2,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status' : 404,
+            'message': 'Not Found'
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
 @app.route('/')
 def index():
     return "Ola mundo!"
@@ -9,3 +20,6 @@ def index():
 @app.route('/api/v1/status')
 def service_status():
     return "Ola mundo2!"
+
+if __name__ == '__main__':
+    aap.run(debug=False)
