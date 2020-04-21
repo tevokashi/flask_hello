7ae712e7258649bea13b4341a58092baf3879ced
Stevan Nogueira <stevan.nogueira@mandic.net.br>
2020-04-21 12:04:46 -0300

    get database status

--- app/api.py
+++ app/api.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)
 
 
 @app.errorhandler(404)
@@ -13,13 +17,25 @@ def not_found(error=None):
     resp.status_code = 404
     return resp
 
+def test_db():
+    try:
+        db.session.query("1").all()
+        return 'OK'
+    except Exception as e:
+        return 'Down'
+
 @app.route('/')
 def index():
     return "Ola mundo!"
 
 @app.route('/api/v1/status')
 def service_status():
-    return "Ola mundo2!"
-
+    data = {}
+    data['API'] = {}
+    data['API']['api'] = "1.0"
+    data['API']['dep'] ={}
+    data['API']['dep']['db-sql'] = test_db()
+    return jsonify(data)
+    
 if __name__ == '__main__':
     app.run(debug=False)
