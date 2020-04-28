701ee086498a2ec08c71fb568afa67371eb4f298
Stevan Nogueira <stevan.nogueira@mandic.net.br>
2020-04-28 19:26:52 -0300

    some tes

--- testes/test.py
+++ testes/test.py
@@ -3,8 +3,9 @@ import sys
 
 url =  'cpro43392.publiccloud.com.br:31500/api/v1/status'
 req = requests.get("http://{}".format(url)).json()
-if req.get('API').get('api') == 1.0:
+if float(req.get('API').get('api')) == 1.0:
     print('OK')
+    print(req)
 else:
     print('ned rolback')
     sys.exit(4)
